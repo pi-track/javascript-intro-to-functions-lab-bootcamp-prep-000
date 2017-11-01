cd56e0b93549ef86f7a79eab5e3a27582ef413a7
patrick.eells@gmail.com <patrick.eells@gmail.com>
2017-11-01 20:51:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === "I love you, Grandma."){
     return "I love you, too."
-  }else (whisper(string) === string) {
+  }
+  else if (whisper(string) === string) {
     return "I can't hear you!"
-  }else (shout(string) === string) {
+  }  
+  else if (shout(string) === string) {
     return "YES INDEED!"
   }
 }
